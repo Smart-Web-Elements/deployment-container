658398585903b32367485fc5d3cce0111ab47209
Luca-Braun <l.braun@s-w-e.com>
2022-04-21 09:41:50 +0200

    Changed build target to dockerhub

--- .space.kts
+++ .space.kts
@@ -4,15 +4,25 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Build Docker image") {
-    docker {
+job("Publish Docker image") {
+    docker("Docker build and push") {
+        env["DOCKERHUB_USER"] = Secrets("dockerhub_user")
+        env["DOCKERHUB_TOKEN"] = Secrets("dockerhub_token")
+
+        beforeBuildScript {
+            content = """
+                B64_AUTH=${'$'}(echo -n ${'$'}DOCKERHUB_USER:${'$'}DOCKERHUB_TOKEN | base64 -w 0)
+                echo "{\"auths\":{\"https://index.docker.io/v1/\":{\"auth\":\"${'$'}B64_AUTH\"}}}" > ${'$'}DOCKER_CONFIG/config.json
+            """
+        }
+
         build {
             context = "docker"
             file = "./docker/Dockerfile"
-            labels["vendor"] = "swe"
+            labels["vendor"] = "mistermarlu"
         }
 
-        push("swe.registry.jetbrains.space/p/swe/deployment/automation") {
+        push("deployment/automation") {
             tags("1.0.\$JB_SPACE_EXECUTION_NUMBER", "latest")
         }
     }
