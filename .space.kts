2ee01d1302a03a12aa00181ecdce4f500574af7f
Luca-Braun <l.braun@s-w-e.com>
2022-09-21 14:29:32 +0200

    [TASK] Adjusted .space.kts

--- .space.kts
+++ .space.kts
@@ -3,8 +3,16 @@
 * This Kotlin-script file lets you automate build activities
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
-/*
+
 job("Publish Docker image") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +Regex("php")
+            }
+        }
+    }
+
     docker("Docker build and push") {
         env["DOCKERHUB_USER"] = Secrets("dockerhub_user")
         env["DOCKERHUB_TOKEN"] = Secrets("dockerhub_token")
@@ -13,6 +21,7 @@ job("Publish Docker image") {
             content = """
                 B64_AUTH=${'$'}(echo -n ${'$'}DOCKERHUB_USER:${'$'}DOCKERHUB_TOKEN | base64 -w 0)
                 echo "{\"auths\":{\"https://index.docker.io/v1/\":{\"auth\":\"${'$'}B64_AUTH\"}}}" > ${'$'}DOCKER_CONFIG/config.json
+                export BRANCH=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'/' -f 3)
             """
         }
 
@@ -22,8 +31,7 @@ job("Publish Docker image") {
         }
 
         push("deployment") {
-            tags("1.0.\$JB_SPACE_EXECUTION_NUMBER", "latest")
+            tags("\$BRANCH", "\$BRANCH.\$JB_SPACE_EXECUTION_NUMBER", "latest")
         }
     }
 }
-*/
